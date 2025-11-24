691caa9d3b0e161dc80860378984ba6f762db4a5
Alejandro Martinez Londoño <alejo-022@hotmail.com>
2025-11-24 10:57:11 -0500

    A;adir fotos de motos y desligar el panel del carro de compras de la barra de navegacion

--- src/assets/db.js
+++ src/assets/db.js
@@ -23,63 +23,63 @@ export const db = [
   {
       id: 4,
       name: 'Kawasaki Ninja ZX-6R',
-      image: 'moto_04',
+      image: 'public\\img\\moto_04.png',
       description: 'Una moto icónica en la categoría supersport, con gran aceleración y estabilidad.',
       price: 65000000, // 65 millones COP
   },
   {
       id: 5,
       name: 'Ducati Monster 821',
-      image: 'moto_05',
+      image: 'public\\img\\moto_05.png',
       description: 'Naked de estilo italiano con un rendimiento excepcional y un sonido inconfundible.',
       price: 72000000, // 72 millones COP
   },
   {
       id: 6,
       name: 'BMW F 900 R',
-      image: 'moto_06',
+      image: 'public\\img\\moto_06.png',
       description: 'Una moto dinámica y moderna, perfecta para recorridos urbanos y escapadas en carretera.',
       price: 62000000, // 62 millones COP
   },
   {
       id: 7,
       name: 'Harley-Davidson Iron 883',
-      image: 'moto_07',
+      image: 'public\\img\\moto_07.png',
       description: 'Estilo custom clásico con un motor contundente y una experiencia de conducción relajada.',
       price: 58000000, // 58 millones COP
   },
   {
       id: 8,
       name: 'KTM Duke 790',
-      image: 'moto_08',
+      image: 'public\\img\\moto_08.png',
       description: 'Potente y ágil, una naked que destaca por su precisión y carácter deportivo.',
       price: 52000000, // 52 millones COP
   },
   {
       id: 9,
       name: 'Triumph Bonneville T100',
-      image: 'moto_09',
+      image: 'public\\img\\moto_09.png',
       description: 'Clásica moderna con una estética retro y un motor suave ideal para paseos tranquilos.',
       price: 55000000, // 55 millones COP
   },
   {
       id: 10,
       name: 'Aprilia RSV4',
-      image: 'moto_10',
+      image: 'public\\img\\moto_10.png',
       description: 'Deportiva de alto rendimiento con tecnología avanzada y excelente comportamiento en pista.',
       price: 95000000, // 95 millones COP
   },
   {
       id: 11,
       name: 'Moto Guzzi V7 III',
-      image: 'moto_11',
+      image: 'public\\img\\moto_11.png',
       description: 'Un diseño vintage combinado con un motor robusto y una conducción cómoda.',
       price: 65000000, // 65 millones COP
   },
   {
       id: 12,
       name: 'Husqvarna Vitpilen 701',
-      image: 'moto_12',
+      image: 'public\\img\\moto_12.png',
       description: 'Minimalista y futurista, una moto ligera y con gran manejo en ciudad.',
       price: 50000000, // 50 millones COP
   },
